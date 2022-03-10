e76538cb1b7da320a261b6083a48f358c5ac6ffc
user Joyeseul <cysysc102@gmail.com>
2022-03-10 18:11:02 +0900

    20220309 section02 - 1 - 요구사항분석 - 도메인모델과 테이블 설계 - 엔티티 클래스 개발1

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -6,8 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpashopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JpashopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JpashopApplication.class, args);
+
+        /* jar 로 빌드 하는 방법.
+        * Terminal 에서
+        * ./gradlew clean build : 새롭게 다시 build
+        * cd D:\Workspace\jpashop\build\libs : 여기에 jpashop-0.0.1-SNAPSHOT.jar 파일이 있다.
+        * java -jar jpashop-0.0.1-SNAPSHOT.jar : jar 파일로 spring 을 실행할 수 있다.
+        * */
+    }
 
 }
